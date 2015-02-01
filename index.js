43435924f395f866a9eceb6346e204356e217c8c
Sylvain Perron <sylvain.perron@protorisk.com>
2015-01-31 19:01:24 -0500

    Done

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 var	Lambdaws 	= require('lambdaws'),
 	Configs		= require('./configs'),
 	Stream 		= require('user-stream'),
+	Lawgs		= require('lawgs'),
 	Rx			= require('rx');
 
 Lambdaws.config(Configs.LAMBDAWS);
 
-function tweetCompute(tweets){
-	var FOOTBALL_LEXICAL_FIELD = [
-		'touchdoun',
-		'fumble',
-		'interception',
-		'turnover',
-		'field goal'
-	];
+function tweetCompute(tweets, callback) {
+	var FOOTBALL_LEXICAL_FIELD = [ 'touchdown', 'fumble', 'interception', 'turnover', 'field goal' ],
+		plainTextTweets = JSON.stringify(tweets),
+		AWS = require('aws-sdk');
 
-	var plainTextTweets = JSON.strignyfy(tweets);
-
-	
-
-
-	FOOTBALL_LEXICAL_FIELD.map(function(term){
-		var termRegEx = new RegExp(term, 'g'),
-			termCount = (termRegEx.match(plainTextTweets) || []).length,
-			threshold = 0.5 * tweets.length
-		
+	var sms = FOOTBALL_LEXICAL_FIELD.filter(function(term){
+		var termRegEx = new RegExp(term, 'gi'),
+			termCount = (plainTextTweets.match(termRegEx) || []).length,
+			threshold = 0.5 * tweets.length;
 		return termCount >= threshold;
-	});
+	}).reduce(function(sms, word) { return sms += word + ' ' }, '');
 
-
-	console.log('data has arrived:', JSON.stringify(data).length);
+	if(sms.length > 0) {
+		new AWS.SNS().publish({
+			Message: 'Superbowl Alert: ' + sms,
+			TopicArn: 'arn:aws:sns:us-east-1:246175073899:Superbowl'
+		}, function() { callback('alerts sent') });
+	} else callback('nothing special');
 }
+
+var cloudedTweetCompute = Lambdaws.create(tweetCompute, [], { name: 'SUPERBOWL'});
+
+var logger = Lawgs('SuperbowlTweets');
+logger.settings.aws = Configs.AWS;
+logger.configure({ });
 
 function mapTweet(tweet){
 	return {
@@ -40,13 +41,15 @@ function mapTweet(tweet){
 		timestamp : tweet.timestamp_ms
 	}
 }
+
 var stream = new Stream(Configs.TWITTER);
 stream.stream({track:'#superbowl'});
 
 Rx.Node.fromEvent(stream, 'data')
 	.map(mapTweet)
-	.bufferWithTimeOrCount(1000, 1000)
+	.do(function(tweet) { logger.log('tweet', tweet) })
+	.bufferWithTimeOrCount(3000, 1000)
 	.filter(function(a){return a.length >0})
-	.subscribe(cloudedTweetCompute);
+	.subscribe(function(tweets) { cloudedTweetCompute(tweets, function() { /* Discard response */ }) });
 
-setTimeout(function() {}, 1000 * 1000); // Keep Alive
+setTimeout(function() {}, 100000 * 100000); // Keep Alive
